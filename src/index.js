ec5f80370c190c0f47b2a70c53b7ce3c8ea5c2cf
pankaj.k <pankaj.k@zohocorp.com>
2025-07-10 00:43:55 +0530

    Custom Api response and Error Handling

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js"; // Import the connectDB function
+import { app } from "./app.js";
 
-dotenv.config({ path: './env' }); // Load environment variables from .env file
-connectDB().then(() => {
-    console.log('Database connection established successfully');
-}).catch((error) => {
-    console.error('Failed to connect to the database:', error);
+dotenv.config({ path: "./env" }); // Load environment variables from .env file
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("Error occurred:", error);
+      throw error; // Re-throw the error to be caught by the outer catch block
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT : ${process.env.PORT}`);
+    });
+    console.log("Database connection established successfully");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the mongo database:", error);
     process.exit(1); // Exit the process with failure
-});
-
+  });
 
 /*
 import mongoose  from "mongoose";
