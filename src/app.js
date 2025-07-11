83f2b59d46f6dcc0dc31c6864a36328950c0a197
pankaj.k <pankaj.k@zohocorp.com>
2025-07-11 22:50:17 +0530

    add user register routes declaration in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from cors;
+import cors from 'cors';
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -21,5 +21,15 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 
 app.use(cookieParser())
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes declaration
+// app.get not worked here because : app and controller in same file here router and controller in different files so, we need to use middleware
+app.use("/api/v1/users", userRouter);  // here after if URL contains `/api/v1/users` then route goes to user.routes and here we have different route based on that we redirect the user like if after `/api/v1/users/regiter` we run regiter controller
+
 
 export {app};
